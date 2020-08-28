255f12c397539895aa45d8a9d93a3aeaf01e846b
VENGAMAMBA <vengamamba.abburi@augmentolabs.com>
2020-08-28 09:39:23 -0700

    Intital Push to Github Repository

---
+++ WindowsForms/WindowsFormsApp2/Maths.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    public static class Maths
+    {
+        public static double add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double multiply(double a, double b)
+        {
+            return a*b;
+        }
+
+        public static double division(double a, double b)
+        {
+            return a/b;
+        }
+
+        public static double subtraction(double a, double b)
+        {
+            return a-b;
+        }
+
+        public static string simpleInterest(double p, double r, double n)
+        {
+            double temp = multiply(p, r);
+
+            return Maths.multiply(temp, n).ToString();
+        }
+
+        public static string compoundInterest(double p, double r, double n)
+        {
+            return Maths.multiply(p, Math.Pow(1 + r, n)).ToString();
+        }
+    }
+}
